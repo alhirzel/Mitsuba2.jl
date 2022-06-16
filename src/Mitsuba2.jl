f558968b19dbb21af2b2d461d7c0fa85f67ec752
Alex Hirzel <alex@hirzel.us>
2022-06-15 22:58:15 -0400

    initial prototype, renders some circles

--- src/Mitsuba2.jl
+++ src/Mitsuba2.jl
@@ -1,5 +1,53 @@
-module Mitsuba2
+baremodule Mitsuba2
 
-greet() = print("Hello World!")
+using Base: @eval
+using PyCall: pyimport
+
+
+
+# list of things to export; each will be allocated then updated in __init__
+let mitsuba_module_name = "mitsuba",
+	variants = pyimport(mitsuba_module_name).variants(),
+	core_imports = [
+		:Bitmap, :FileResolver, :filesystem, :Float, :Frame3f, :Logger,
+		:Spectrum, :Ray3f, :Struct, :Thread, :Vector2f, :Vector3f,
+	],
+    render_imports = [
+		:BSDF, :BSDFContext, :Emitter, :Film, :Film, :ImageBlock, :Integrator,
+		:PhaseFunction, :Sampler, :Scene, :Sensor, :Shape, :SurfaceInteraction3f,
+		:Texture, :Volume,
+	],
+	all_imports = [core_imports..., render_imports...]
+
+	# allows callers to use it
+	@eval variants() = $(variants)
+
+	for variant in variants
+		@eval baremodule $(Symbol(variant))
+
+			using Base: copy!
+			using PyCall: PyNULL, @py_str, pyimport
+
+			# this is the general pattern that is used to expose mitsuba
+			load_dict = load_file = load_string = PyNULL()
+			export load_dict, load_file, load_string
+
+			$( [:(const $(sym) = PyNULL()) for sym in all_imports]... )
+			$( [:(export $(sym)) for sym in all_imports]... )
+
+			# set all of the PyNULLs to the real PyObjects
+			function __init__()
+				mitsuba = pyimport($(mitsuba_module_name))
+				mitsuba.set_variant($(variant))
+				$( [:(copy!($(sym), mitsuba.core.$(sym))) for sym in core_imports]... )
+				$( [:(copy!($(sym), mitsuba.render.$(sym))) for sym in render_imports]... )
+				copy!(load_dict, mitsuba.core.xml.load_dict)
+				copy!(load_file, mitsuba.core.xml.load_file)
+				copy!(load_string, mitsuba.core.xml.load_string)
+				return mitsuba
+			end
+		end
+	end
+end
 
 end # module
---
+++ test/runtests.jl
@@ -0,0 +1,63 @@
+using ColorTypes: RGB
+using ImageInTerminal
+using Mitsuba2
+import Mitsuba2.scalar_rgb as mitsuba
+using PyCall: PyObject, PyNULL, pyimport
+using Test
+
+
+
+const test_scene_xml = """
+<scene version="2.0.0">
+	<shape type="sphere">
+		<bsdf type="diffuse"/>
+	</shape>
+	<integrator type="path"/>
+	<emitter type="point">
+		<rgb name="intensity" value="200,0,0"/>
+		<point name="position" x="2" y="0" z="2"/>
+	</emitter>
+	<emitter type="point">
+		<rgb name="intensity" value="0,0,200"/>
+		<point name="position" x="2" y="0" z="-2"/>
+	</emitter>
+	<sensor type="perspective">
+		<transform name="to_world">
+			<lookat origin="4, 0, 0" target="0, 0, 0" up="0, 0, 1"/>
+		</transform>
+		<film type="hdrfilm">
+			<integer name="width" value="32"/>
+			<integer name="height" value="32"/>
+		</film>
+		<sampler type="ldsampler">
+			<integer name="sample_count" value="256" />
+		</sampler>
+	</sensor>
+</scene>
+"""
+
+
+
+scene = mitsuba.load_string(test_scene_xml)
+
+@testset "load a basic scene" begin
+	@test scene != PyNULL()
+	@test scene isa PyObject
+end
+
+sensor = first(scene.sensors())
+
+@testset "render the scene" begin
+	@test scene.integrator().render(scene, sensor) # true means success
+	@test sensor.film().size() == [32, 32]
+end
+
+img = sensor.film().bitmap(raw=true)
+
+# TODO - factor this into the package
+img = img.convert(mitsuba.Bitmap.PixelFormat.RGB, mitsuba.Struct.Type.Float32, srgb_gamma=false)
+img = mapslices(Base.splat(RGB), pyimport("numpy").array(img; copy=false); dims=(3,))[:, :, 1]
+
+display(img)
+println("")
+
